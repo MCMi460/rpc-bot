44ebf2a463e699685b39bd07eda22e04b6d350fe
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-08-20 21:18:19 -0500

    Please work

--- bot.py
+++ bot.py
@@ -39,15 +39,13 @@ async def on_ready():
             name = 'your game suggestions'
         )
     )
-    global NRP_Guild
-    NRP_Guild = discord.utils.get(bot.guilds, id = 1012066992817193001)
-    if not NRP_Guild:
+    if not discord.utils.get(bot.guilds, id = NRP_Guild):
         raise Exception('expected to be in Nintendo Rich Presence guild')
 
 @bot.event
 async def on_member_join(member):
     guild = member.guild
-    if guild.id == NRP_Guild.id and guild.system_channel is not None:
+    if guild.id == NRP_Guild and guild.system_channel is not None:
         await guild.system_channel.send(
             'Welcome to %s, %s! If you want a quick tutorial on using NSO-RPC or 3DS-RPC, go to <#1126310378981294211>.\nOr, if you\'d like a special role (like 3DS Updates or NSO Updates), see <id:customize>!' % (
                 member.guild.name, member.mention
